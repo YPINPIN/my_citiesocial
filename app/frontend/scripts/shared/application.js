a631c30832ea5168e449a4fc26e75ebd97d54aaf
YPINPIN <yping611@gmail.com>
2022-07-12 17:51:12 +0800

    add flash

--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -10,3 +10,14 @@ document.addEventListener('turbolinks:load', () => {
     });
   });
 });
+
+// notification delete
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
