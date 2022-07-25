19fe96d3dbfe7d85ce2aa7d04d3bf9610a849519
YPINPIN <yping611@gmail.com>
2022-07-25 18:24:54 +0800

    add SKU part 2

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,6 +6,7 @@ class Admin::ProductsController < Admin::BaseController
 
   def new
     @product = Product.new
+    @product.skus.build
   end
 
   def create
@@ -45,7 +46,10 @@ class Admin::ProductsController < Admin::BaseController
                                     :list_price, 
                                     :sell_price,
                                     :on_sell,
-                                    :description)
+                                    :description,
+                                    skus_attributes: [
+                                      :id, :spec, :quantity, :_destroy
+                                    ])
   end
 
 end
