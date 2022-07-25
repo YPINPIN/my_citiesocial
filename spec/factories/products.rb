92a119b6961b1f7e95b229f9a10e87d8bbde2af1
YPINPIN <yping611@gmail.com>
2022-07-25 15:55:44 +0800

    17 - add model Product

---
+++ spec/factories/products.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    vendor { nil }
+    list_price { "9.99" }
+    sell_price { "9.99" }
+    on_sell { false }
+    code { "MyString" }
+    deleted_at { "2022-07-25 15:51:51" }
+  end
+end
