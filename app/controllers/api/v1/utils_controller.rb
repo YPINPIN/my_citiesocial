3ae1f5af76b4e9920fed7ac7d485c5fa3614a8f0
YPINPIN <yping611@gmail.com>
2022-07-27 17:00:49 +0800

    subscription part 2

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::UtilsController < ApplicationController
   def subscribe
-    render json: { ststus: 'ok'}
+    email = params['subscribe']['email']
+
+    sub = Subscribe.new(email: email)
+
+    if sub.save
+      render json: { status: 'ok', email: email}
+    else
+      render json: { status: 'duplicated', email: email}
+    end 
   end
 end
