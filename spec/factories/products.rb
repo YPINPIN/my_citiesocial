9308b57414d7d1a6f764b2f315404ca88db93e26
YPINPIN <yping611@gmail.com>
2022-08-17 00:31:08 +0800

    shopping cart with TDD part 5

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    vendor { nil }
-    list_price { "9.99" }
-    sell_price { "9.99" }
+    name { Faker::Name.name }
+    list_price { Faker::Number.between(from: 10, to: 50) }
+    sell_price { Faker::Number.between(from: 1, to: 50) }
     on_sell { false }
-    code { "MyString" }
-    deleted_at { "2022-07-25 15:51:51" }
+
+    vendor
+    category
   end
 end
