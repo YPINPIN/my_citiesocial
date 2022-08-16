6c2f861d4c3a998804cb61f35a2899c6e94ecf23
YPINPIN <yping611@gmail.com>
2022-08-16 16:35:36 +0800

    Category part 5

--- app/frontend/scripts/backend/sortable.js
+++ app/frontend/scripts/backend/sortable.js
@@ -1,4 +1,5 @@
 import Sortable from 'sortablejs';
+import Rails from '@rails/ujs';
 
 document.addEventListener('turbolinks:load', () => {
   let el = document.querySelector('.sortable-items');
@@ -7,7 +8,22 @@ document.addEventListener('turbolinks:load', () => {
     Sortable.create(el, {
       onEnd: event => {
         let [model, id] = event.item.dataset.item.split('_');
-        console.log(model, id)
+        let data = new FormData();
+        data.append("id", id);
+        data.append("from", event.oldIndex);
+        data.append("to", event.newIndex);
+
+        Rails.ajax({
+          url: '/admin/categories/sort',
+          data,
+          type: 'PUT',
+          success: (response) => {
+            console.log(response);
+          },
+          error: function (err) {
+            console.log(err);
+          }
+        });
       }
     })
   }
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     root 'products#index' # /admin
     resources :products
     resources :vendors, except: [:show]
-    resources :categories, except: [:show]
+    resources :categories, except: [:show] do
+      collection do
+        put :sort # PUT /admin/categories/sort
+      end
+    end
   end
 
   namespace :api do
