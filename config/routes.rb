0b4bef2acf845a62c1bfe1923ceaa518f38b600c
YPINPIN <yping611@gmail.com>
2022-08-18 17:47:51 +0800

    Using shopping cart part 6

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show] # /categories/2
+  
+  resource :cart, only: [:show, :destroy]
 
   namespace :admin do
     root 'products#index' # /admin
