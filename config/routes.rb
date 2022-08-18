88be423307052a7905e2ac6a63d2f2797fd1a796
YPINPIN <yping611@gmail.com>
2022-08-19 00:01:07 +0800

    Using shopping cart part 9

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :orders, expect: [:new, :edit, :update, :destroy]
+
   namespace :admin do
     root 'products#index' # /admin
     resources :products
